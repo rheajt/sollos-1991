9276c5d4575c03fa3a790daac362c4d5d6e93ff1
jordan rhea <rheajt@gmail.com>
2019-10-11 18:21:29 +0800

    changed to jsx files

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `sollos-1991`,
+    description: `kids on bikes group`,
+    author: `@rheajt`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
